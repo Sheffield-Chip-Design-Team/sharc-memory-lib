d0582500a5ca9288d7206b8e9aeed25899585d7c
James <162901067+Enjimneering@users.noreply.github.com>
2026-03-17 21:45:51 +0000

    restructure memory lib directory structure

--- sim/verilator/build/verilated_cov.d
+++
@@ -1,9 +0,0 @@
-verilated_cov.o: \
-  /usr/local/Cellar/verilator/5.038/share/verilator/include/verilated_cov.cpp \
-  /usr/local/Cellar/verilator/5.038/share/verilator/include/verilatedos.h \
-  /usr/local/Cellar/verilator/5.038/share/verilator/include/verilated_cov.h \
-  /usr/local/Cellar/verilator/5.038/share/verilator/include/verilated.h \
-  /usr/local/Cellar/verilator/5.038/share/verilator/include/verilated_config.h \
-  /usr/local/Cellar/verilator/5.038/share/verilator/include/verilated_types.h \
-  /usr/local/Cellar/verilator/5.038/share/verilator/include/verilated_funcs.h \
-  /usr/local/Cellar/verilator/5.038/share/verilator/include/verilated_cov_key.h
